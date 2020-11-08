321a0c6bf213a165cd4b31a0ff15b9e710903b56
Alex Sukhitashvili <sukhitashvilialex2@gmail.com>
2020-11-08 18:57:49 +0400

    feat: api connect

---
+++ ByteCoin/Model/Coin.swift
@@ -0,0 +1,17 @@
+//
+//  Coin.swift
+//  ByteCoin
+//
+//  Created by Alex Sukhitashvili on 11/8/20.
+//  Copyright © 2020 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct CoinModel {
+    let time: String
+    let asset_id_base: String
+    let asset_id_quote: String
+    let rate: Double
+}
